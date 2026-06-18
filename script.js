a422a107d8b63b164f315ceb359a8e4860b0f8e2
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-06-18 12:51:03 -0500

    added a second question

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Declare variables below to save the different divs of your story.
-let option1
-
-
-
-
+let option1 = document.querySelector(".option-one");
+let option2 = document.querySelector(".option-two");
+let option3 = document.querySelector(".option-three");
+let option4 = document.querySelector(".option-four");
+let image1 = document.querySelector(".image-one");
+let image2 = document.querySelector(".image-two");
+let question1 = document.querySelector(".question-one");
+let question2 = document.querySelector(".question-two");
+let option2Screen = document.querySelector(".option-two-screen");
+let option1Screen = document.querySelector(".option-one-screen");
+let opening = document.querySelector(".story-opening");
+let endScreen = document.querySelector(".end-screen");
 
 
 
@@ -12,15 +19,37 @@ let option1
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+option1.addEventListener('click', function () {
+    option1.style.display = "none";
+    option2.style.display = "none";
+     opening.style.display = "none";
+    // image1.style.display = "none";
+    // question1.style.display = "none";
+    question2.style.display = "block";
+    option1Screen.style.display = "block";
+    option3.style.display= "block";
+});
+option2.addEventListener('click', function () {
+    option1.style.display = "none";
+    option2.style.display = "none";
+     opening.style.display = "none";
+    // image1.style.display = "none";
+    // question1.style.display = "none";
+    endScreen.style.display = "block";
+    option2Screen.style.display = "block";
+  
+});
 
-// });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+option3.addEventListener('click', function(){
+     option1.style.display = "none";
+    option2.style.display = "none";
+     opening.style.display = "none";
+    // image1.style.display = "none";
+    // question1.style.display = "none";
+    endScreen.style.display = "none";
+    option2Screen.style.display = "none";
+});
+option4.addEventListener("click", function(){
 
-// });
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+});
